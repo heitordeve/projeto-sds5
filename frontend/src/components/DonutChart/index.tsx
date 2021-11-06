d03be7ec4870cd27ee31e894dacfe55ee59c8aa5
heitordeve <heitor.developer@outlook.com>
2021-11-06 11:40:21 -0300

    Dev - DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,38 +1,41 @@
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
-    labels : string[];
-    series : number[];
+    labels: string[];
+    series: number[];
 }
 
 const DonutChart = () => {
 
-    let chartData: ChartData =  { labels: [], series: [] };
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            chartData = { labels: myLabels, series: mySeries };
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, [])
 
-            console.log(chartData);
-        });
+
 
     //const mockData = {
-        //series: [477138, 499928, 444867, 220426, 473088],
-        //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //series: [477138, 499928, 444867, 220426, 473088],
+    //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
-    
+
     const options = {
         legend: {
             show: true
         }
-    }    
+    }
 
     return (
         <Chart
@@ -40,7 +43,7 @@ const DonutChart = () => {
             series={chartData.series}
             type="donut"
             height="240"
-        />        
+        />
     );
 }
 
